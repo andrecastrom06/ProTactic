e7736e8d1222fd0c916913a79b60e29e1d720099
andrecastrom06 <alcms@cesar.school>
2025-06-05 09:41:56 -0300

    spring iniatializer

---
+++ protactic/src/test/java/com/protactic/protactic/ProtacticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.protactic.protactic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtacticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
