cbed1ca3546dd6c152858e843ffbc090f1548137
Rodrigo Torres <rtmr@cesar.school>
2025-06-08 20:52:24 -0300

    crud de clube completo

---
+++ protactic/src/main/java/com/protactic/protactic/repository/ClubeRepository.java
@@ -0,0 +1,7 @@
+package com.protactic.protactic.repository;
+
+import com.protactic.protactic.model.Clube;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubeRepository extends JpaRepository<Clube, String> {
+}
