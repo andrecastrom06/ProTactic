de1c5309bb48d343cbec5d4963a7bd049975643a
andrecastrom06 <alcms@cesar.school>
2025-09-10 13:18:49 -0300

    inicio de configuração do cucumber

---
+++ dominio/principal-bdd/src/test/java/dev/protactic/sgb/principal/dominio/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package dev.protactic.sgb.principal.dominio;
+
+import static io.cucumber.core.options.Constants.PLUGIN_PROPERTY_NAME;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("com.protactic.protactic.principal.dominio")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
